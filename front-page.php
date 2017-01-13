c8be00aa917f6b7c6daa3d8c22bb18a64068406a
kindlingscript <inflamedmuse@gmail.com>
2017-01-12 22:33:32 -0600

    Landing page introduction work for mobile

--- front-page.php
+++ front-page.php
@@ -5,8 +5,8 @@ get_header();
 ?>
 
 <section class="hero">
-  <!-- <img src="<php bloginfo('stylesheet_directory'); ?>/img/nashville.jpg" class="hero-image"> -->
-  <h1 class="header__large-title">We're the Nashville chapter of the Nation Stuttering Association, meeting once a month to support each other and talk about stuttering.</h1>
+  <h1 class="header__large-title header--intro">We're the Nashville chapter of the Nation Stuttering Association, meeting once a month to support each other and talk about stuttering.</h1>
+  <button class="btn btn--find-out-more">Find out more</button>
 </section>
 
 <?php get_footer(); ?>
