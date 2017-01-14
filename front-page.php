3aacc5093e550237b1b9425ddf751f045071816b
kindlingscript <inflamedmuse@gmail.com>
2017-01-13 21:37:25 -0600

    Footer work and more styling

--- front-page.php
+++ front-page.php
@@ -9,25 +9,27 @@ get_header();
   <button class="btn btn--find-out-more">Find out more</button>
 </section>
 
-<section class="section section__blog">
+<section class="section section__blog wrapper">
   <h2 class="header__large-title header--blue">Catch up with our blog →</h2>
   <p class="font__content">Beyond our regular monthly meetings, our members make posts about their experiences with stuttering. Whether they themselves stutter, or they are or have been a SLP (speech-language pathologist), each person has had personal/professional experience with stuttering and offer unique insights of it. Feel free to read a few posts, comment on some, or submit your own!</p>
 </section>
 
-<section class="section section__location">
+<section class="section section__location wrapper">
   <div class="section__screen-width">
     <?php while ( have_posts() ): the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; ?>
   </div>
-  <h2 class="header__large-title header--blue">Where we meet</h2>
-  <p class="font__content">On the third Tuesday of each month, our chapter of the National Stuttering Association meets at Glen Leven Presbyterian Church off of Franklin Road in Nashville, TN. Meetings typically start at 6pm and end at around 8pm.</p>
-  <p class="font__content">For more information, click on the red marker inside the map.</p>
+  <div class="section__location--content">
+    <h2 class="header__large-title header--blue">Where we meet</h2>
+    <p class="font__content">On the third Tuesday of each month, our chapter of the National Stuttering Association meets at Glen Leven Presbyterian Church off of Franklin Road in Nashville, TN. Meetings typically start at 6pm and end at around 8pm.</p>
+    <p class="font__content">For more information, click on the red marker inside the map.</p>
+  </div>
 </section>
 
 <section class="section section__footer">
-  <div class="links__more-info">
-    <h2>For more information:</h2>
+  <div class="links__more-info wrapper">
+    <h2 class="font__white-bg">For more information:</h2>
     <ul>
       <li>
         <a href="http://westutter.org" target="_blank">National Stuttering Association</a>
